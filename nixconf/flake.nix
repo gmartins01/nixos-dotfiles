661ad419838625d78233933e846178742c1dcea5
Gonçalo Martins <goncallo.c.martins@gmail.com>
2024-09-10 23:06:27 +0100

    Plasma configuration

--- nixconf/flake.nix
+++ nixconf/flake.nix
@@ -8,10 +8,17 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, plasma-manager, ... }@inputs:
     let
+      username = "gmartins";
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       devShells = import ./shell/devShells.nix { pkgs = pkgs; };
@@ -31,8 +38,15 @@
           specialArgs = { inherit inputs; };
           modules = [
             ./hosts/desktop/configuration.nix
-            inputs.home-manager.nixosModules.default
             ./modules/nixos
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+              
+              home-manager.users."${username}" = import ./hosts/desktop/home.nix;
+            }
           ];
         };
       };
---
+++ nixconf/modules/home-manager/plasma/default.nix
@@ -0,0 +1,123 @@
+{ config,lib, pkgs, ... }:
+
+{
+
+  options = {
+    plasma.enable = lib.mkEnableOption "Enable Plasma";
+  };
+
+  config = lib.mkIf config.plasma.enable {
+    programs.plasma = {
+      enable = true;
+
+      workspace = {
+        lookAndFeel = "org.kde.breezedark.desktop";
+        colorScheme = "BreezeDark";
+        cursor = {
+          theme = "Breeze_Light";
+          size = 24;
+        };
+        iconTheme = "Papirus-Dark";
+      };
+
+      shortcuts = {
+        "services/org.kde.konsole.desktop"."_launch" = ["Meta+Return" "Ctrl+Alt+T"];
+        "services/plasma-manager-commands.desktop"."launch-konsole" = "Meta+Return";
+        "kwin"."Window Close" = ["Meta+C" "Alt+F4,Alt+F4,Close Window"];
+        "plasmashell"."show-on-mouse-pos" = "Meta+V";
+        "services/org.kde.krunner.desktop"."RunClipboard" = ["Alt+Shift+F2" "Meta+R"];
+      };
+
+      krunner = {
+        position = "center";        
+        historyBehavior = "enableSuggestions";
+      };
+
+      spectacle = {
+        shortcuts = {
+          captureActiveWindow = "Meta+Print";
+          captureCurrentMonitor = "Print";
+          captureEntireDesktop = "Shift+Print";
+          captureRectangularRegion = "Meta+Shift+Print";
+          captureWindowUnderCursor = "Meta+Ctrl+Print";
+          launch = "Meta+Shift+S";
+          launchWithoutCapturing = "Meta+Alt+S";
+          recordRegion = "Meta+Shift+R";
+          recordScreen = "Meta+Alt+R";
+          recordWindow = "Meta+Ctrl+R";
+        };
+      };
+      
+      windows = {
+        allowWindowsToRememberPositions = true;
+      };
+
+      /*panels = [
+        {
+          location = "bottom";
+          floating = true;
+          hiding = "none";
+          screen = 0;
+          widgets = [
+            {
+              kickoff = {
+                sortAlphabetically = true;
+                icon = "nix-snowflake";
+              };
+            }
+
+            {
+              iconTasks = {
+                launchers = [
+                  "applications:org.kde.dolphin.desktop"
+                  "applications:org.keepassxc.KeePassXC.desktop"
+                  "applications:firefox.desktop"
+                  "applications:code.desktop"
+                  "applications:org.kde.konsole.desktop"
+                ];
+              };
+            }
+
+            "org.kde.plasma.marginsseparator"
+            
+            {
+              systemTray.items = {
+                shown = [
+                  "org.kde.plasma.networkmanagement"
+                  "org.kde.plasma.volume"
+                
+                ];
+
+                hidden = [  
+                  "org.kde.plasma.clipboard"
+                  "org.kde.plasma.battery"
+                  "org.kde.plasma.brighness"
+                  "org.kde.plasma.notifications"
+                  "org.kde.plasma.printmanager"
+                  "org.kde.plasma.removabledevices"
+                  "org.kde.plasma.mediacontroller"
+                  "org.kde.plasma.kdeconnect"
+                  "org.kde.kscreen"
+                  "org.kde.plasma.devicenotifier"
+                  "org.kde.plasma.manage-inputmethod"
+                  "org.kde.plasma.lockkeystatus"
+                ];
+
+              };
+            }
+
+            {
+              digitalClock = {
+                calendar.firstDayOfWeek = "monday";
+                time.format = "24h";
+              };
+            }
+          ];
+          
+        }
+      ];*/
+
+    };
+  };
+
+}
