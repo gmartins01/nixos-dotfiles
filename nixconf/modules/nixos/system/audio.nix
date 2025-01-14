97ecf49f71520452fa8adbcac0eb7ada92752bf4
Gonçalo Martins <goncallo.c.martins@gmail.com>
2025-01-14 23:05:44 +0000

    Fix audio hyprbar

--- nixconf/modules/nixos/system/audio.nix
+++ nixconf/modules/nixos/system/audio.nix
@@ -1,7 +1,8 @@
 { pkgs, lib, ... }:
 
 {
-  # Enable sound with pipewire.
+  environment.systemPackages = [ pkgs.alsa-utils ];
+
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -9,12 +10,16 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 44100;
+        "default.clock.quantum" = 512;
+        "default.clock.min-quantum" = 512;
+        "default.clock.max-quantum" = 512;
+      };
+    };
   };
 
 }
