2e08762aaf3480a91eb411ae5324e702388c3f46
Gonçalo Martins <goncallo.c.martins@gmail.com>
2025-01-16 21:21:25 +0000

    Gnome keyring

--- nixconf/modules/nixos/windowManager/hyprland.nix
+++ nixconf/modules/nixos/windowManager/hyprland.nix
@@ -17,9 +17,29 @@
     qt5.qtwayland
 
     libsForQt5.kservice # to fix opening files with default applications
+
+    libsForQt5.kwallet-pam
+    libsForQt5.kwallet
   ];
 
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.sddm.enableGnomeKeyring = true;
-  environment.sessionVariables = { NIXOS_OZONE_WL = "1"; };
+
+  #security.pam.services.sddm.enableKwallet = true;
+  #security.pam.services.sddm = {
+  #  enableKwallet = true;
+  #  text = ''
+  #    auth include login
+  #  '';
+  #};
+  #security.pam.services.kwallet = {
+  #  name = "kwallet";
+  #  enableKwallet = true;
+  #};
+
+  #security.pam.services.root99.enableKwallet = true;
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
 }
