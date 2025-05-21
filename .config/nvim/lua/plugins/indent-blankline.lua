30e638ffaefb88147ef305206bbc56664c25eb6a
Gonçalo Martins <goncallo.c.martins@gmail.com>
2025-05-21 21:20:47 +0100

    Indent color change

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,23 @@
 return {
-	{ -- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
+	-- Add indentation guides even on blank lines
+	"lukas-reineke/indent-blankline.nvim",
 
-		-- See `:help ibl`
-		main = "ibl",
-		event = "VeryLazy",
-		opts = {},
-	},
+	-- See `:help ibl`
+	main = "ibl",
+	event = "VeryLazy",
+	opts = {},
+
+	config = function()
+		local highlight = {
+			"IndentScope",
+		}
+		local hooks = require("ibl.hooks")
+
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "IndentScope", { fg = "#7f849c" })
+		end)
+
+		vim.g.rainbow_delimiters = { highlight = highlight }
+		require("ibl").setup({ scope = { highlight = highlight, show_start = false } })
+	end,
 }
