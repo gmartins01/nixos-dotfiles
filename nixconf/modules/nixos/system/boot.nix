0e3ce308ac0f7706468285972af91dd149a96c1c
Gonçalo Martins <goncallo.c.martins@gmail.com>
2026-05-29 21:04:33 +0100

    chore: update

--- nixconf/modules/nixos/system/boot.nix
+++ nixconf/modules/nixos/system/boot.nix
@@ -8,7 +8,7 @@
   boot.loader.systemd-boot.enable = false;
   boot.supportedFilesystems = ["ntfs"];
 
-  boot.loader.grub.enable = true;
+  boot.loader.grub.enable = false;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
@@ -18,6 +18,17 @@
   boot.kernelParams = ["amdgpu.ppfeaturemask=0xffffffff"]; # Corectrl
   # boot.kernelPackages = pkgs.linuxPackages_cachyos;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.loader.limine.enable = true;
+  boot.loader.limine.secureBoot.enable = true;
+  boot.loader.limine.extraEntries = ''
+    /Windows
+      protocol: efi
+      path: uuid(4a055065-8fe2-4cea-bbbb-54fd9e2a1d6d):/EFI/Microsoft/Boot/bootmgfw.efi
+  '';
+
+  environment.systemPackages = [pkgs.sbctl];
+
   /*
     boot.extraModulePackages = [
     (amdgpu-kernel-module.overrideAttrs (_: {
